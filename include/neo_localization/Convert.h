60a22006ca5c16394cd625a75133e16d14779a63
Frederik Plahl <plfr1013@h-ka.de>
2022-05-11 11:34:08 +0200

    Fix tf2_geometry_msgs error

--- include/neo_localization/Convert.h
+++ include/neo_localization/Convert.h
@@ -11,7 +11,7 @@
 #include <neo_localization/GridMap.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/thread.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp> 
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h> 
 #include <tf2/transform_datatypes.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/utils.h>
